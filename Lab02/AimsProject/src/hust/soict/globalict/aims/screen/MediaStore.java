28a739aef74f2c3dacb8675a2616eec2a7642804
nam861836 <namgt283@gmail.com>
2023-06-16 14:38:39 +0700

    inlab

---
+++ Lab02/AimsProject/src/hust/soict/globalict/aims/screen/MediaStore.java
@@ -0,0 +1,13 @@
+package hust.soict.globalict.aims.screen;
+
+import hust.soict.globalict.aims.media.Media;
+
+import javax.swing.*;
+
+public class MediaStore extends JPanel {
+    private Media media;
+    public MediaStore(Media media){
+        this.media = media;
+    }
+
+}
